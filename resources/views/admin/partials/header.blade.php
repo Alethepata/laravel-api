6189b2767559072687c21db7eda20c257f0994ee
Alethepata <alessia.patane@gmail.com>
2023-12-12 12:30:26 +0100

    [Add]  Auth::user()->name/ search

--- resources/views/admin/partials/header.blade.php
+++ resources/views/admin/partials/header.blade.php
@@ -1,13 +1,30 @@
 <header>
-    <nav class="navbar bg-black">
+    <nav class="navbar bg-black d-flex">
+
         <div class="container-fluid">
+
           <a class="navbar-brand text-light"  href="{{route('home')}}" target="_blank">Vai al sito</a>
-          <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button class="btn btn-outline-light" type="submit">
-                <i class="fa-solid fa-arrow-right-from-bracket"></i>
-            </button>
-        </form>
+
+          <div class="d-flex ">
+
+            <div class="container-fluid">
+              <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              </form>
+            </div>
+
+            <p class="text-light mx-4">{{Auth::user()->name}}</p>
+
+            <form action="{{ route('logout') }}" method="POST">
+              @csrf
+              <button class="btn btn-outline-light" type="submit">
+                  <i class="fa-solid fa-arrow-right-from-bracket"></i>
+              </button>
+          </form>
+
+          </div>
+
         </div>
+
       </nav>
 </header>
