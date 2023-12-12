c23a12fa8ad3d1eefcef92c408c28aa07fd21003
Alethepata <alessia.patane@gmail.com>
2023-12-12 18:59:15 +0100

    [add] user_id

--- database/seeders/ProjectsTableSeeder.php
+++ database/seeders/ProjectsTableSeeder.php
@@ -8,6 +8,7 @@ use Illuminate\Database\Seeder;
 use App\Models\Project;
 use App\Models\Type;
 use Faker\Generator as Faker;
+use App\Models\User;
 
 class ProjectsTableSeeder extends Seeder
 {
@@ -21,6 +22,7 @@ class ProjectsTableSeeder extends Seeder
         for($i = 0; $i < 10; $i++){
             $new_project = new Project();
             $new_project->type_id = Type::inRandomOrder()->first()->id;
+            $new_project->user_id = User::inRandomOrder()->first()->id;
             $new_project->title = $faker->sentence();
             $new_project->slug = Project::generateSlug($new_project->title);
             $new_project->start_date = $faker->date();
